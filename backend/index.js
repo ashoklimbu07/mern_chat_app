8cad34266b95082bd911c43f6fe271dc4d64a19a
ashoklimbu07 <limashok3.70@gmail.com>
2026-05-14 13:12:39 +0545

    init : simple node server

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+
+
+const port = 3000
+const app=express()
+
+
+//middlewares
+
+
+//routes
+
+
+//start server
+app.listen(port,()=>{
+    console.log(`Server running on http://localhost:${port}`)
+})
+
