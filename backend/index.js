2468bb27c08511e645f90730c5a59f3c772d7965
ashoklimbu07 <limashok3.70@gmail.com>
2026-05-14 15:39:22 +0545

    configure : prettier code fromater

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,24 @@
-import express from "express"
-import dotenv from "dotenv"
-import { connectMongoDb } from "./config/db.js"
-import userRoutes from "./routes/user.routes.js"
-import authRoutes from "./routes/auth.routes.js"
+import express from "express";
+import dotenv from "dotenv";
+import { connectMongoDb } from "./config/db.js";
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT || 3000
-const app = express()
+const port = process.env.PORT || 3000;
+const app = express();
 
 // middlewares
-app.use(express.json())
+app.use(express.json());
 
 // routes
-app.use("/api/users", userRoutes)
-app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 // start server after DB connects
 connectMongoDb().then(() => {
     app.listen(port, () => {
-        console.log(`Server running on http://localhost:${port}`)
-    })
-})
+        console.log(`Server running on http://localhost:${port}`);
+    });
+});
